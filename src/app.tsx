f53914ab5dfa364936adb82846465454e1f73987
glider <glider1967@protonmail.com>
2024-12-20 21:44:55 +0900

    add examples

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,37 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 import "./app.css";
-import init, { eval_script } from "../rscript/pkg";
+import init, { eval_script } from "../pkg";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
+const FUNCTIONS = `let twice = lambda(f, x) {
+    f(f(x))
+};
+let multFour = twice(lambda(x) {x*2});
+multFour(7)`;
+
+const FIBONACCI = `let fib = lambda(n) {
+    if(n == 0 || n == 1) {
+        1
+    } else {
+        fib(n-1) + fib(n-2)
+    }
+};
+fib(10)`;
+
+const VAR_AND_SCOPE = `let i = false;
+let f = lambda(i, j) {
+    let q = if(!j) {
+        i
+    } else {
+        i + 2
+    };
+    q / 2
+};
+f(8, i)`;
+
 export function App() {
-  const [code, setCode] = useState("let x = 1; x + 3");
-  const [result, setResult] = useState("");
+  const [code, setCode] = useState(FUNCTIONS);
+  const [result, setResult] = useState("evaluate to see result");
   const editorRef = useRef<HTMLDivElement>(null);
   const monacoRef = useRef<monaco.editor.IStandaloneCodeEditor>();
 
@@ -16,7 +42,7 @@ export function App() {
     // init of code editor
     if (editorRef.current === null) return;
     monacoRef.current = monaco.editor.create(editorRef.current, {
-      value: "let x = 1;\nx + 3",
+      value: FUNCTIONS,
       language: "plaintext",
       theme: "vs",
       minimap: {
@@ -38,11 +64,32 @@ export function App() {
   return (
     <>
       <h1>tiny script language written in rust</h1>
-      <div ref={editorRef} style={{ width: "80%", height: "500px" }}></div>
-      <div class="card">
-        <button onClick={() => setResult(eval_script(code))}>evaluate</button>
+      <div>
+        Examples:
+        <button
+          onClick={() => monacoRef.current?.setValue(FUNCTIONS)}
+          class="buttons"
+        >
+          functions
+        </button>
+        <button
+          onClick={() => monacoRef.current?.setValue(FIBONACCI)}
+          class="buttons"
+        >
+          fibonacci
+        </button>
+        <button
+          onClick={() => monacoRef.current?.setValue(VAR_AND_SCOPE)}
+          class="buttons"
+        >
+          variables and scopes
+        </button>
       </div>
-      <div>{result}</div>
+      <div ref={editorRef} style={{ width: "100%", height: "500px" }}></div>
+      <button onClick={() => setResult(eval_script(code))} class="buttons">
+        evaluate
+      </button>
+      <div class="output">{result}</div>
     </>
   );
 }
