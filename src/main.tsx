fd916129361e405194121b60320b0fc3bccf4648
glider <glider1967@protonmail.com>
2024-12-24 13:56:27 +0900

    set basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { render } from 'preact'
-import { App } from './app.tsx'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app.tsx";
+import "./index.css";
+import { ErrorBoundary, LocationProvider } from "preact-iso";
 
-render(<App />, document.getElementById('app')!)
+render(
+  <LocationProvider scope="/rscript-playground">
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </LocationProvider>,
+  document.getElementById("app")!
+);
